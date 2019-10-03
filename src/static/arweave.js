83af86dde94a1350c394bace3a7fee0a0012894e
Maciej Krüger <mkg20001@gmail.com>
2019-10-03 17:16:18 +0200

    feat: new sw-power-router

--- src/static/arweave.js
+++ src/static/arweave.js
@@ -2,25 +2,20 @@
 
 const arlang = require('arlang')
 const Boom = require('@hapi/boom')
-const Mimos = require('@hapi/mimos')
 
-const mimos = new Mimos()
-
-function getMimeType (path) {
-  return mimos.path(path).type
-}
-
-function fetchArweaveFile (arweave, id, path) {
+function fetchArweaveFile (h, arweave, id, path) {
   return id
-  // return new Response(await fetchedRes.blob(), { status: fetchedRes.status, statusText: fetchedRes.statusText, headers: { 'content-type': getMimeType(path) } })
+  /* return h.response(await fetchedRes.blob())
+    .status(fetchedRes.status),
+    .message(fetchedRes.statusText) */
 }
 
 module.exports = async ({address, id}, arweave) => {
-  return async ({path}) => {
+  return async ({path}, h) => {
     const transactions = await arweave.arql(arlang('& (= app $1) (& (= from $2) (= path $3))', {lang: 'sym', params: [id, address, path]}))
     if (transactions.length) {
       let transaction = transactions[0]
-      return fetchArweaveFile(arweave, transaction, path)
+      return fetchArweaveFile(h, arweave, transaction, path)
     } else {
       return Boom.notFound(`No arweave entry under ${JSON.stirngify({app: id, from: address, path})}`)
     }
