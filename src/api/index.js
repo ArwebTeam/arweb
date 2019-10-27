d865fdd317521bd296aecdb35011e45aa68faec3
Maciej Krüger <mkg20001@gmail.com>
2019-10-27 13:10:15 +0100

    chore: upgrade deps

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,14 @@ const Account = require('./account')
 const Swarm = require('./swarm')
 const Config = require('./config')
 const Info = require('./info')
+const TXQueue = require('./txqueue')
 
-module.exports = async ({prefix, account, swarm, config, info} = {}, {router: {route}, user, arweave}) => {
+module.exports = async ({prefix, account, swarm, config, info, txqueue} = {}, {router: {route}, user, arweave}) => {
   const p = {route, user, prefix, arweave}
 
   await Account(account, p)
   await Swarm(swarm, p)
   await Config(config, p)
   await Info(info, p)
+  await TXQueue(txqueue, p)
 }
